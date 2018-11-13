0e47f094f1ed95c00ab735100d1f83c4ea60673c
CodyBakerOnshore <40472466+CodyBakerOnshore@users.noreply.github.com>
2018-11-12 22:43:13 -0500

    File Name Fix

---
+++ KitsuneBot/CommandModules/CommandsModule.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+// Create a module with no prefix
+public class InfoModule : ModuleBase
+{
+    // ~say hello -> hello
+    [Command("say"), Summary("Echos a message.")]
+    public async Task Say([Remainder, Summary("The text to echo")] string echo)
+    {
+        // ReplyAsync is a method on ModuleBase
+        await ReplyAsync(echo);
+    }
+}
+
+
+public class SquareModule : ModuleBase
+{
+    // ~sample square 20 -> 400
+    [Command("square"), Summary("Squares a number.")]
+    public async Task Square([Summary("The number to square.")] int num)
+    {
+        // We can also access the channel from the Command Context.
+        await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+    }
+}
+
+public class UserInfoModule : ModuleBase
+{
+    // ~sample userinfo --> foxbot#0282
+    // ~sample userinfo @Khionu --> Khionu#8708
+    // ~sample userinfo Khionu#8708 --> Khionu#8708
+    // ~sample userinfo Khionu --> Khionu#8708
+    // ~sample userinfo 96642168176807936 --> Khionu#8708
+    // ~sample whois 96642168176807936 --> Khionu#8708
+    [Command("userinfo"), Summary("Returns info about the current user, or the user parameter, if one passed.")]
+    [Alias("user", "whois")]
+    public async Task UserInfo([Summary("The (optional) user to get info for")] IUser user = null)
+    {
+        if(user == null)
+        {
+            user = Context.User;
+        }
+        var userInfo = user ?? Context.Client.CurrentUser;
+        await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+    }
+}
