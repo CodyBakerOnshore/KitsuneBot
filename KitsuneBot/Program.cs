44c70795b0a0a6f58f0d8be8e6ccf3c0e1ec520e
CodyBakerOnshore <40472466+CodyBakerOnshore@users.noreply.github.com>
2018-11-13 21:00:48 -0500

    Tech Debt Fix, misc unneeded code removal

--- KitsuneBot/Program.cs
+++ KitsuneBot/Program.cs
@@ -1,29 +1,28 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Threading.Tasks;
-using Discord;
+﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using KitsuneBot.CommandHandler;
 using KitsuneBot.Properties;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace KitsuneBot
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        private static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
         private DiscordSocketClient _client;
-        
+
         private const int _delay = -1;
 
         private CommandService commands;
 
 #pragma warning disable 1998
+
         public async Task MainAsync()
-        #pragma warning restore 1998
+#pragma warning restore 1998
         {
             _client = new DiscordSocketClient();
             commands = new CommandService();
@@ -33,7 +32,6 @@ namespace KitsuneBot
             await InstallCommands();
 
             _client.Log += Log;
-
 
             string Token = Resources.DiscordToken;
 
@@ -66,7 +64,7 @@ namespace KitsuneBot
             if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
             // Create a Command Context
             var context = new CommandContext(_client, message);
-            // Execute the command. (result does not indicate a return value, 
+            // Execute the command. (result does not indicate a return value,
             // rather an object stating if the command executed successfully)
             var result = await commands.ExecuteAsync(context, argPos/*, service*/);
             if (!result.IsSuccess)
@@ -76,57 +74,11 @@ namespace KitsuneBot
         /*
          * Creation of logger to write errors console following Discord.Net Doc Gettting Started guide replace with better logger later!
          */
+
         private Task Log(LogMessage _message)
         {
             Console.WriteLine(_message.ToString());
             return Task.CompletedTask;
-        }
-
-        // Create a module with no prefix
-        public class InfoModule : ModuleBase<CommandContext>
-        {
-            // say hello world returns: hello world
-            [Command("say"), Summary("Echos a Message")]
-            public Task SayAsAsync([Remainder, Summary("The Text To Echo")] string echo) => ReplyAsync(echo);
-
-            // ReplyAsync is a method on ModuleBase
-        }
-
-        // Create a module with the 'sample' prefix
-        [Group("sample")]
-        public class SampleModule : ModuleBase<CommandContext>
-        {
-            // sample square 20 returns: 400
-            [Command("square")]
-            [Summary("Squares a number.")]
-            public async Task SquareAsync([Summary("The number to square.")] int num)
-            {
-                // We can also access the channel from the Command Context.
-                await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
-            }
-
-            // sample userinfo returns: foxbot#0282
-            // sample userinfo @Khionu returns: Khionu#8708
-            // sample userinfo Khionu#8708 returns: Khionu#8708
-            // sample userinfo Khionu returns: Khionu#8708
-            // sample userinfo 96642168176807936 returns: Khionu#8708
-            // sample whois 96642168176807936 returns: Khionu#8708
-            [Command("userinfo")]
-            [Summary("Returns info about the current user, or the user parameter, if one passed.")]
-            [Alias("user", "whois")]
-            public async Task UserInfoAsync([Summary("The (optional) user to get info from")] SocketUser user = null)
-            {
-                if(user != null)
-                {
-                    var userInfo = user;
-                    await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
-                }
-                else
-                {
-                    var userInfo = Context.Client.CurrentUser;
-                    await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
-                }
-            }
         }
     }
 }
